592e3d59489bcd9ab475db2f004860005a13fb51
Rodesigned <rodesigned@att.net>
2018-01-27 00:30:25 -0500

    files tonight

---
+++ assets/javascript/slide.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+
+//slide js 
+var openNav;
+var closeNav;
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "350px";
+    document.getElementById("#main").style.marginLeft = "350px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("#main").style.marginLeft= "0";
+}
+
+	
